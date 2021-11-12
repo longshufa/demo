6092e6d424bed6cded6e49ffb69c116e78732a52
longsf <lshufa@126.com>
2021-11-12 15:43:41 +0800

    修复

--- src/tp51/pay/service/wechat/pay/SubPubAndMiniPay.php
+++ src/tp51/pay/service/wechat/pay/SubPubAndMiniPay.php
@@ -48,7 +48,7 @@ class SubPubAndMiniPay extends BaseWechat {
         if( isset($payData["sub_openid"]) ){
             $input->SetSubOpenid($payData['sub_openid']);
         }
-        if( $payData['is_set_profit_sharing'] ){
+        if( isset($payData['is_set_profit_sharing']) && $payData['is_set_profit_sharing'] == 'Y' ){
             $input->SetProfitSharing("Y");
         }
         $wxOrder = WxPayApi::unifiedOrder($config, $input );
